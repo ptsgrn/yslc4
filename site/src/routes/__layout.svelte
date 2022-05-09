914da5a6c8bf8742251ad211ed59c89549e6cd8c
Patsagorn <49602385+ptsgrn@users.noreply.github.com>
2022-05-10 02:39:37 +0700

    update

--- site/src/routes/__layout.svelte
+++ site/src/routes/__layout.svelte
@@ -1,5 +1,62 @@
 <script>
   import "../app.css";
+  import LogoIMG from "../assets/logo_uncrop.png";
+  import DtacSafeInternetIMG from '../assets/dtacSIC.png'
+  import YSLCIMG from '../assets/yscl-dtac-1024x574.png'
+  import IconEmailOutline from 'svelte-material-icons/EmailOutline.svelte'
+  import LineSVGIMG from '../assets/line-brands.svg'
 </script>
-
+<nav class="bg-blue-400">
+  <div class="max-w-5xl mx-auto">
+    <div class="flex items-center justify-evenly">
+      <a href="/" class="flex items-center">
+        <span class="text-md p-1">
+          หน้าแรก
+        </span>
+      </a>
+      <a href="/" class="flex items-center">
+        <span class="text-md p-1">
+          ผู้ขอรับบริจาค
+        </span>
+      </a>
+      <a href="/" class="flex items-center">
+        <span class="text-md p-1">
+          ยื่นขอรับบริจาค
+        </span>
+      </a>
+      <a href="/" class="flex items-center">
+        <span class="text-md p-1">
+          ประกาศ
+        </span>
+      </a>
+      <a href="/" class="flex items-center">
+        <span class="text-md p-1">
+          เข้าสู่ระบบ
+        </span>
+      </a>
+    </div>
+  </div>
+</nav>
 <slot />
+<footer class="flex flex-row bg-gray-400 justify-center items-center p-3 max-w-5xl">
+  <img src={LogoIMG} alt="Logo" class="h-14 w-14 rounded-full mx-4" />
+  <img src={DtacSafeInternetIMG} alt="dtacSafeInternet logo" class="h-14 mx-4" />
+  <img src={YSLCIMG} alt="Young Safe Internet Leader CyberCamp" class="h-14 mx-4"/>
+  <ul class="flex flex-col justify-center mx-4 text-xs">
+    <!-- <div class="flex">
+      <a href='mailto:thermtem.org@gmail.com' class="">
+        <IconEmailOutline size="20" />
+      </a>
+      <img src={LineSVGIMG} alt="line icon" class="h-[18px]" />
+    </div> -->
+    <li>
+      Email:&nbsp;<a href='mailto:thermtem.org@gmail.com'>thermtem.org@gmail.com</a>
+    </li>
+    <li>
+      Line&nbsp;Official:&nbsp;<a href='https://line.me/ti/p/~@287htijc'>@287htijc</a>
+    </li>
+    <li>
+      Facebook:&nbsp;<a href='https://www.facebook.com/thermtem.org'>Thermtem.org</a>
+    </li>
+  </ul>
+</footer>
--- site/tailwind.config.cjs
+++ site/tailwind.config.cjs
@@ -1,12 +1,24 @@
 
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'mainpage': "url('/pexels-ben-mack.jpg')",
-      },
-
+  theme: {},
+  extend: {
+    backgroundImage: {
+      'mainpage': "url('/pexels-ben-mack.jpg')",
+    },
+    colors: {
+      yellow: '#FAE57C',
+      blue: {
+        100: '#d6f0ff',
+        200: '#99daff',
+        300: '#5cc4ff',
+        400: '#34b6ff',
+        500: '#2991cc',
+        600: '#1f6d99',
+        700: '#1a5b7f',
+        800: '#0f364c',
+        900: '#051219'
+      }
     },
   },
   plugins: [],
