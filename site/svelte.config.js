f22e39919305db7c6b3b04f488c5e0d2dbe5a3bd
Patsagorn <49602385+ptsgrn@users.noreply.github.com>
2022-07-29 11:30:31 +0700

    dep(sveltekit)!: update svelte to 1.0.0-next.396
    format: all

--- site/svelte.config.js
+++ site/svelte.config.js
@@ -1,20 +1,9 @@
 import adapter from '@sveltejs/adapter-auto';
-import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-		vite: {
-			resolve: {
-				alias: {
-					'@': path.resolve('./src'),
-					'@components': path.resolve('./src/components'),
-					'@lib': path.resolve('./src/lib'),
-					'@assets': path.resolve('./src/lib/assets')
-				}
-			}
-		}
+		adapter: adapter()
 	}
 };
 
---
+++ site/vite.config.js
@@ -0,0 +1,17 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import path from 'node:path';
+
+/** @type {import('vite').UserConfig} */
+const config = {
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			'@': path.resolve('./src'),
+			'@components': path.resolve('./src/components'),
+			'@lib': path.resolve('./src/lib'),
+			'@assets': path.resolve('./src/lib/assets')
+		}
+	}
+};
+
+export default config;
